07846b9f6316473d4a4bcd786ed2fdf9125cc755
Ioannis Maragkakis <giannis.marar@hotmail.com>
2023-12-31 13:34:20 +0200

    changed the clear-data-files-folder script

--- mysite/clear-data-files-folder.py
+++ mysite/clear-data-files-folder.py
@@ -1,9 +1,15 @@
 import os, shutil
-from flask import current_app
 
-data_file_folder = os.path.join(current_app.root_path, "data_files")
+# Get the directory of the current script
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Specify the relative path to the "data_files" folder
+data_file_folder = os.path.join(script_directory, "data_files")
+
+# delete the folder
 shutil.rmtree(data_file_folder)
 
+# re-make the folder
 try:
     os.mkdir(data_file_folder)
 except:
