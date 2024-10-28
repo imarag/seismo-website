9ffdbe47dc30396b50e1d7c2e0404e710b51b8e8
imarag <giannis.marar@hotmail.com>
2024-10-28 02:13:23 +0200

    added all the article components into the ArticleComponents.js file with multiple named exports

--- reactapp/src/pages/articles/Matplotlib.js
+++ reactapp/src/pages/articles/Matplotlib.js
@@ -1,4 +1,9 @@
-import ReactEmbedGist from 'react-embed-gist';
+import {
+    ArticleTitle, 
+    ArticleSubTitle, 
+    ArticleImage, 
+    ArticleCode, 
+    ArticleGist} from "../../components/ArticleComponents"
 
 import SubplotMosaic from "../../img/subplot_mosaic.png"
 import Windows from "../../img/windows.png"
@@ -12,44 +17,6 @@ import MatplotlibEmptyFigure from "../../img/matplotlib_empty_figure.png"
 import PandasFourierDataframe from "../../img/pandas_fourier_dataframe.png"
 import MatplotlibFigureGrid from "../../img/matplotlib_figure_grid.png"
 import MatplotlibFigureGridTightLayout from "../../img/matplotlib_figure_grid_tight_layout.png"
-
-function ArticleTitle({ text }) {
-    return (
-        <h1 className="text-5xl font-normal text-blue-950 text-start mb-5 mt-12">{ text }</h1>
-    )
-}
-
-function ArticleSubTitle({ text }) {
-    return (
-        <h2 className="text-3xl font-normal text-blue-950 text-start mb-5 mt-12">{ text }</h2>
-    )
-}
-
-function ArticleImage({ imageUrl, caption }) {
-    return (
-        <figure className="my-6">
-            <img src={imageUrl} className="block mx-auto mb-2"/>
-            <figcaption className="text-center text-blue-800 font-normal">{ caption }</figcaption>
-        </figure>
-    )
-}
-
-function ArticleCode({ text }) {
-    return (
-        <>
-            <code className="text-gray-800  text-base px-1 font-normal">{text}</code>
-        </>
-    )
-}
-
-function ArticleGist({ gist }) {
-    return (
-        <div className="my-6">
-            <ReactEmbedGist gist={gist} />
-        </div>
-    )
-}
-
 
 export default function Matplotlib() {
   return (
