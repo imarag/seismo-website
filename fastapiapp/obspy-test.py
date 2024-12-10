9cecc130668e4e857513d6c90f8a823bfa4cdf55
imarag <giannis.marar@hotmail.com>
2024-12-10 21:42:22 +0200

    changes

--- fastapiapp/obspy-test.py
+++
@@ -1,8 +0,0 @@
-from obspy.core import read 
-
-
-st = read(r"C:\Users\giann\Downloads\5c795995-e6ad-482c-a7e5-78558af740a5.mseed")
-tr = st[0]
-tr.stats.station = ""
-print(tr.stats)
-print(tr.get_id())
