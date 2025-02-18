46e8c9a091e790dc25fe361a40a64ce60030c82a
imarag <giannis.marar@hotmail.com>
2025-02-19 00:10:25 +0200

    changed the message component (window at the bottom right) to white background, some shadow and base-100 because the type wan't applied the background color

--- remixapp/app/components/ui/Message.jsx
+++ remixapp/app/components/ui/Message.jsx
@@ -1,11 +1,15 @@
 import { IoMdClose } from "react-icons/io";
+import { MdError } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
 
 export default function Message({ type, text, setError, setSuccess }) {
+    const messageBase = "flex flex-row items-center"
     const messagePosition = "fixed end-8 bottom-8 z-40"
     const messageText = "text-sm text-start font-semibold"
     const messageSize = "w-80 p-6"
-    const messageBackground = `alert alert-${type} opacity-100`
-    const messageClassName = `${messagePosition} ${messageText} ${messageSize} ${messageBackground}`
+    const messageBackground = `bg-base-100 shadow-2xl opacity-100`
+    const messageBorder = "border border-grey-300 rounded-lg"
+    const messageClassName = `${messageBase} ${messageBorder} ${messagePosition} ${messageText} ${messageSize} ${messageBackground}`
     return (
         <div className={messageClassName}>
             <button 
@@ -17,7 +21,18 @@ export default function Message({ type, text, setError, setSuccess }) {
             >
                 <IoMdClose />
             </button>
-            { text }
+            <div className="flex-grow-0 flex-shrink-0">
+                {
+                    type === "error" ? (
+                        <MdError className="size-8"/>
+                    ) : (
+                        <FaCheckCircle className="size-8"/>
+                    )
+                }
+            </div>
+            <p className="flex-grow">
+                { text }
+            </p>
         </div>
     )
 }
