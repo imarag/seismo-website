7e081a898329ea0d121bd005bd515703a84cec3b
imarag <giannis.marar@hotmail.com>
2025-05-17 15:59:23 +0300

    changesss

---
+++ seismo-frontend/dist/_astro/Spinner.D8BFhGps.js
@@ -0,0 +1,1 @@
+import{j as n}from"./jsx-runtime.D_zvdyIk.js";function a(){return n.jsx("p",{className:"text-center",children:n.jsx("span",{className:"loading loading-ring loading-lg"})})}export{a as S};
